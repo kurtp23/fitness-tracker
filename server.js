872c3b4d47e9518207888180a59d1a364112ffb9
kurtp23 <kurt_phillips@live.com>
2021-03-18 13:35:30 -0700

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 const apiHtml = require("./routes/api-routes.js");
